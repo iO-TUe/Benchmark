cf045635525a464c186227ff04b72fe4318e73d9
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-03-18 10:09:20 +0100

    Code/jsdoc cleanup

--- tests/utils.js
+++ tests/utils.js
@@ -2,21 +2,22 @@ import { mkdirSync, readFileSync, readdirSync, rmSync } from "fs";
 import { spawnSync } from 'node:child_process';
 import { beforeAll, bench } from "vitest";
 
-const iterations = 10,
-    warmupIterations = 5,
-    implementations = [
-        ['React', 'https://io-2imc05.web.app/'],
-        ['Qwik', 'https://qwiiik.web.app/'],
-    ],
-    runs = Object.fromEntries(implementations.map(([name]) => [name, []]))
+const iterations = 10
+const warmupIterations = 5
+const implementations = [
+    ['React', 'https://io-2imc05.web.app/'],
+    ['Qwik', 'https://qwiiik.web.app/'],
+], runs = Object.fromEntries(implementations.map(([$]) => [$, []]))
 
 /**
- * Runs a benchmark of {@link fn} for all implementations provided. 
+ * Runs a benchmark of {@link fn} for all {@link implementations} provided. 
+ * 
+ * Sample size is set by {@link iterations} after {@link warmupIterations} warmup runs.
  * 
  * Before the benchmark, the tmp dir is cleared and all chrome processes are closed.
  * 
  * @param {Function} fn The benchmark function.
- * @param {boolean} dry Use results of the last benchmark instead of running a new one.
+ * @param {boolean} [dry] Use results of the last benchmark instead of running a new one.
  */
 function setup(fn, dry = false) {
     implementations.forEach(([name, url]) =>
