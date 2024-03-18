a99ec948f0bcdf8e5c6d648b36530f732654c580
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-03-18 17:22:46 +0100

    Fix JSON output

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        reporters: ['json', 'verbose'],
         outputFile: "./tmp/bench.json",
         testTimeout: 0,
         maxConcurrency: 1,
