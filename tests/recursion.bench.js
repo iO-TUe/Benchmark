9031fc9d1a8ecf868e8ac7b942a84c696842b145
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-03-20 10:37:09 +0100

    Usage functions to utils + add throttling

--- tests/recursion.bench.js
+++ tests/recursion.bench.js
@@ -1,12 +1,11 @@
 import { appendFileSync, readFileSync, writeFileSync } from 'fs';
 import { generateReport, startFlow } from 'lighthouse';
 import { computeMedianRun } from 'lighthouse/core/lib/median-run';
-import { spawnSync } from 'node:child_process';
-import { launch } from 'puppeteer';
+import { launch, PredefinedNetworkConditions } from 'puppeteer';
 import { afterAll } from "vitest";
-import { runs, setup, warmupIterations as wi } from './utils';
+import { runs, setup, warmupIterations as wi, computeMedianCPU, usage } from './utils';
 
-setup(flows, true)
+setup(flows)
 
 afterAll(() => Object.entries(runs).forEach(([name, results]) => {
     const lhr = results.slice(wi).map(flow => flow[0].lhr)
@@ -23,8 +22,9 @@ afterAll(() => Object.entries(runs).forEach(([name, results]) => {
 }))
 
 async function flows(name, url) {
-    const browser = await launch({ headless: 'new' })
-    const page = await browser.newPage()
+    const page = await launch({ headless: 'new' }).newPage()
+    await page.emulateCPUThrottling(2)
+    await page.emulateNetworkConditions(PredefinedNetworkConditions['Fast 3G'])
 
     const flow = await startFlow(page, {
         config: {
@@ -76,69 +76,6 @@ async function flows(name, url) {
     writeFileSync(`./tmp/lighthouse/${name + iter}.html`, generateReport(json, 'html'))
     runs[name].push(json.steps)
 
-    await browser.close()
+    await page.browser.close()
 }
 
-/**
- * Use Tasklist to retrieve all chrome.exe processes that have used at least 10s of CPU time.
- * The process Status=unknown is used to filter out visible UI threads.
- * 
- * @returns {string[][]} Tuples of PID, Mem. usage and CPU time.
- */
-function usage() {
-    return spawnSync('tasklist', ['/fo', 'csv', '/v',
-        '/fi', 'ImageName eq chrome.exe',
-        '/fi', 'CPUTime gt 00:00:10',
-        '/fi', 'Status eq unknown'
-    ], { encoding: 'utf-8' }).stdout.split('\n').slice(1, -1)
-        .map(s => s.replaceAll('"', '').split(',')).map(a => [a[1], a[4], a[7]])
-}
-
-
-/**
- * Calculate the run closest to the median of CPU Time and Memory Usage. 
- * The calculation is made using the Euclidean distance.
- * 
- * @param {Array<string>} usage CSV output of {@link usage()}
- * @returns {[number, number]} The median CPU and memory usage pair
- */
-function computeMedianCPU(usage) {
-    const cpuMem = usage.map(s => [
-        s.split(';')[2].split(':').reduce((t, s, i, a) =>
-            t += i + 1 < a.length ? s * 60 : +s, 0),
-        +s.split(';')[1].replace(' K', '')
-    ])
-    const medianCpu = median(cpuMem.map(t => t[0]))
-    const medianMem = median(cpuMem.map(t => t[1]))
-    return cpuMem.sort((a, b) => computeMedianDistance(a, medianCpu, medianMem) -
-        computeMedianDistance(b, medianCpu, medianMem))[0]
-}
-
-/**
- * Compute the median value of the provided array.
- * 
- * @param {Array<number>} array 
- * @returns {number} Median value
- */
-function median(array) {
-    const sorted = array.sort((a, b) => a - b)
-    const half = Math.floor(sorted.length / 2)
-
-    return sorted.length % 2 ? sorted[half]
-        : (sorted[half - 1] + sorted[half]) / 2
-}
-
-/**
- * Calculate the distance of the CPU and memory pair to the median.
- * 
- * @param {[number, number]} cpu
- * @param {number} medianCpu
- * @param {number} medianMem 
- * @returns {number} Distance to the median
- */
-function computeMedianDistance(cpu, medianCpu, medianMem) {
-    const distanceCpu = medianCpu - cpu[0]
-    const distanceMem = medianMem - cpu[1]
-
-    return distanceCpu ** 2 + distanceMem ** 2
-}
