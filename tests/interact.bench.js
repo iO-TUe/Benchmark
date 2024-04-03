6f78d0760b6c16f6b5cd2c0a0e131e8a875bee06
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-04-03 17:50:36 +0200

    Only increment after paint

--- tests/interact.bench.js
+++ tests/interact.bench.js
@@ -7,7 +7,7 @@ const base = `./tmp/${basename(__filename).split('.')[0]}`
 setup(flows, base)
 
 async function flows(name, url) {
-    const browser = await launch({ headless: false })
+    const browser = await launch({ devtools: true })
     const page = await browser.newPage()
     const flow = await startFlow(page, flowConfig)
 
@@ -15,10 +15,10 @@ async function flows(name, url) {
 
     await flow.startTimespan({ name: 'Increase' })
     let value
-    do {
-        value = await page.$eval("[class*=value]", el => el.textContent)
+    while ((value = await page.$eval("[class*=value]", el => el.textContent)) < 55) {
         await page.click('button[aria-label="+"')
-    } while (value < 75)
+        await page.waitForFunction((value) => document.querySelector("[class*=value]").textContent == ++value, {}, value)
+    }
     await flow.endTimespan()
 
     saveResults(base, name, flow)
