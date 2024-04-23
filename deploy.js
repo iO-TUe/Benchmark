aafe1d0e6245de2c84d818b7a9147c1447512cfb
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-04-23 10:47:15 +0200

    Add global deploy

---
+++ deploy.js
@@ -0,0 +1,19 @@
+import { spawn } from "child_process";
+import { readdirSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const path = dirname(dirname(fileURLToPath(import.meta.url)))
+
+/**
+ * Deploy all project in the parent folder
+ */
+readdirSync(path).forEach(dir => {
+    if (dir && dir != 'benchmark') {
+        spawn('npm.cmd', ['run', 'deploy'], {
+            env: process.env,
+            cwd: join(path, dir),
+            stdio: 'inherit'
+        })
+    }
+})
--- generator/counter.gen.js
+++ generator/counter.gen.js
@@ -1,6 +1,6 @@
 import { existsSync, mkdirSync, readFile, writeFile } from "fs"
 
-const n = 5
+const duplicates = 5
 const comp = 'counter'
 const path = 'src/components'
 
@@ -11,9 +11,9 @@ const path = 'src/components'
  * @param {string[]} tags
  * @param {string} ext
  * @param {string[]} comment
- * @param {string} imports
+ * @param {string[]} amble
  */
-export function generate(name, ext, tags, comment, imports, component, props) {
+export function generate(name, ext, tags, comment, amble, component, props) {
     const fullpath = `../${name}/${path}/${comp}`
     const fullpathExt = `${fullpath}.${ext}`
     readFile(fullpathExt, (err, file) => {
@@ -25,15 +25,15 @@ export function generate(name, ext, tags, comment, imports, component, props) {
         if (oc.startsWith(comment[0] + '#OC#')) oc = oc.split('#OC#')[1]
         let ec = oc.replaceAll('##', comment[1]).replaceAll('./', '../')
 
-        for (let i = 0; i < n; i++) {
+        for (let i = 0; i < duplicates; i++) {
             writeFile(`${fullpath}/${comp}${i}.${ext}`, ec, writeCB)
         }
 
-        writeFile(fullpathExt, `${comment[0]}#OC#${oc.replaceAll(comment[1], '##')}#OC#${comment[1]}${imports}
-${duplicate((i) => `import ${comp.toUpperCase()}${i} from './${comp}/${comp}${i}.${ext}'`, '')}
+        writeFile(fullpathExt, `${comment[0]}#OC#${oc.replaceAll(comment[1], '##')}#OC#${comment[1]}${amble[0] ?? ''}
+${duplicate((i) => `import ${comp.toUpperCase()}${i} from './${comp}/${comp}${i}${ext != 'tsx' ? `.${ext}` : ''}'`)}
 
-${component} ${tags[0]}
-  ${duplicate((i) => `<${comp.toUpperCase()}${i} ${props} />`, '\t')}
+${component}${amble[1] ?? ' '}${tags[0]}
+  ${duplicate((i) => `<${comp.toUpperCase()}${i} ${props} />`)}
 ${tags[1]}`, writeCB)
     })
 }
@@ -43,10 +43,23 @@ function writeCB(err) {
     if (err) throw err
 }
 
-/**
- * @param {(i: number) => string} str
- * @param {string} indent
- */
-function duplicate(str, indent) {
-    return [...Array(n).keys()].reduce((p, c, i) => p + (i > 0 ? indent : '') + str(c) + (i < n - 1 ? '\n' : ''), '')
+/** @param {(i: number) => string} str */
+function duplicate(str) {
+    return [...Array(duplicates).keys()].reduce((p, c, i) => p + str(c) + (i < duplicates - 1 ? '\n' : ''), '')
 }
+
+generate('qwik', 'tsx', ['<>', '</>)'], ['/**', '*/'], ["\nimport { component$ } from '@builder.io/qwik'"],
+    'export default component$((props: { initialValue: number, maxValue: number, recurse: boolean }) =>',
+    'initialValue={props.initialValue} maxValue={props.maxValue} recurse={props.recurse}')
+generate('react', 'tsx', ['<>', '</>'], ['/**', '*/'], [],
+    'export default (props: { initialValue: number, maxValue: number, recurse: boolean }) =>',
+    'initialValue={props.initialValue} maxValue={props.maxValue} recurse={props.recurse}')
+generate('solid', 'tsx', ['<>', '</>'], ['/**', '*/'], [],
+    'export default (props: { initialValue: number, maxValue: number, recurse: boolean }) =>',
+    'initialValue={props.initialValue} maxValue={props.maxValue} recurse={props.recurse}')
+generate('svelte', 'svelte', ['', ''], ['<!--', '-->'], ['<script lang="ts">', '\n</script>\n'],
+    'export let initialValue: number;\nexport let maxValue: number;\nexport let recurse: boolean;',
+    'initialValue={initialValue} maxValue={maxValue} recurse={recurse}')
+generate('vue', 'vue', ['<template>', '</template>'], ['<!--', '-->'], ['<script setup lang="ts">', '\n</script>\n\n'],
+    'const props = defineProps<{ initialValue: number, maxValue: number, recurse: boolean }>()',
+    ':initialValue="props.initialValue" :maxValue="props.maxValue" :recurse="props.recurse"')
