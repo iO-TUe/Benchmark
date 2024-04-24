bd4e2fcb10a6a284fb204c163a36cbf3395db44a
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-04-24 23:46:10 +0200

    Update gen

--- generator/counter.gen.js
+++ generator/counter.gen.js
@@ -1,6 +1,6 @@
-import { existsSync, mkdirSync, readFile, writeFile } from "fs"
+import { existsSync, mkdirSync, readFile, rmSync, writeFile } from "fs"
 
-const duplicates = 5
+const duplicates = 50
 const comp = 'counter'
 const path = 'src/components'
 
@@ -10,26 +10,23 @@ const path = 'src/components'
  * @param {string} component
  * @param {string[]} tags
  * @param {string} ext
- * @param {string[]} comment
  * @param {string[]} amble
  */
-export function generate(name, ext, tags, comment, amble, component, props) {
+export function generate(name, ext, tags, amble, component, props) {
     const fullpath = `../${name}/${path}/${comp}`
     const fullpathExt = `${fullpath}.${ext}`
     readFile(fullpathExt, (err, file) => {
         if (err) throw (err)
 
-        if (!existsSync(fullpath)) mkdirSync(fullpath)
+        if (existsSync(fullpath)) rmSync(fullpath, { recursive: true, force: true })
+        mkdirSync(fullpath)
 
-        let oc = file.toString()
-        if (oc.startsWith(comment[0] + '#OC#')) oc = oc.split('#OC#')[1]
-        let ec = oc.replaceAll('##', comment[1]).replaceAll('./', '../')
-
+        let oc = file.toString().replaceAll('./', '../')
         for (let i = 0; i < duplicates; i++) {
-            writeFile(`${fullpath}/${comp}${i}.${ext}`, ec, writeCB)
+            writeFile(`${fullpath}/${comp}${i}.${ext}`, oc, writeCB)
         }
 
-        writeFile(fullpathExt, `${comment[0]}#OC#${oc.replaceAll(comment[1], '##')}#OC#${comment[1]}${amble[0] ?? ''}
+        writeFile(fullpath + '.gen.' + ext, `${amble[0] ?? ''}
 ${duplicate((i) => `import ${comp.toUpperCase()}${i} from './${comp}/${comp}${i}${ext != 'tsx' ? `.${ext}` : ''}'`)}
 
 ${component}${amble[1] ?? ' '}${tags[0]}
@@ -48,18 +45,18 @@ function duplicate(str) {
     return [...Array(duplicates).keys()].reduce((p, c, i) => p + str(c) + (i < duplicates - 1 ? '\n' : ''), '')
 }
 
-generate('qwik', 'tsx', ['<>', '</>)'], ['/**', '*/'], ["\nimport { component$ } from '@builder.io/qwik'"],
+generate('qwik', 'tsx', ['<>', '</>)'], ["\nimport { component$ } from '@builder.io/qwik'"],
     'export default component$((props: { initialValue: number, maxValue: number, recurse: boolean }) =>',
     'initialValue={props.initialValue} maxValue={props.maxValue} recurse={props.recurse}')
-generate('react', 'tsx', ['<>', '</>'], ['/**', '*/'], [],
+generate('react', 'tsx', ['<>', '</>'], [],
     'export default (props: { initialValue: number, maxValue: number, recurse: boolean }) =>',
     'initialValue={props.initialValue} maxValue={props.maxValue} recurse={props.recurse}')
-generate('solid', 'tsx', ['<>', '</>'], ['/**', '*/'], [],
+generate('solid', 'tsx', ['<>', '</>'], [],
     'export default (props: { initialValue: number, maxValue: number, recurse: boolean }) =>',
     'initialValue={props.initialValue} maxValue={props.maxValue} recurse={props.recurse}')
-generate('svelte', 'svelte', ['', ''], ['<!--', '-->'], ['<script lang="ts">', '\n</script>\n'],
+generate('svelte', 'svelte', ['', ''], ['<script lang="ts">', '\n</script>\n'],
     'export let initialValue: number;\nexport let maxValue: number;\nexport let recurse: boolean;',
     'initialValue={initialValue} maxValue={maxValue} recurse={recurse}')
-generate('vue', 'vue', ['<template>', '</template>'], ['<!--', '-->'], ['<script setup lang="ts">', '\n</script>\n\n'],
+generate('vue', 'vue', ['<template>', '</template>'], ['<script setup lang="ts">', '\n</script>\n\n'],
     'const props = defineProps<{ initialValue: number, maxValue: number, recurse: boolean }>()',
     ':initialValue="props.initialValue" :maxValue="props.maxValue" :recurse="props.recurse"')
